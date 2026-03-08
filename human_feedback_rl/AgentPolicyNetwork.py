7c7663d3881726edfbcb15d18315f0364528db8a
andreazhang <andrea.zhang@polimi.mail.com>
2026-03-08 23:18:01 +0100

    added agent policy network

---
+++ human_feedback_rl/AgentPolicyNetwork.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+
+class AgentPolicyNetwork(nn.Module):
+
+    def __init__(self, obs_dim, n_actions):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(obs_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_actions),
+        )
+
+    def forward(self, x):
+        return self.net(x)
